131d14888130c942df593d89ae21b949d5b7574b
StumbDev <stumbledeveloper@hotmail.com>
2024-09-28 09:57:02 -0300

    add base (incomplete)

---
+++ src/components/_nav.jsx
@@ -0,0 +1,23 @@
+import * as react from "react-dom";
+
+function NavSetting() {
+    <button
+      class="setting.btn"
+      onClick={
+        "setting()"
+      }
+    ></button>
+}
+
+function NavBtnBack() {
+    // make that part later
+}
+
+export default function MyApp() {
+    return (
+      <div>
+        <h1>Welcome to my app</h1>
+        <MyButton />
+      </div>
+    );
+}
---
+++ src/components/_setting.jsx
@@ -0,0 +1,9 @@
+import * as react from "react-dom";
+
+export default function MyApp() {
+    return (
+      <div>
+        <h1>Settingsd</h1>
+      </div>
+    );
+}
